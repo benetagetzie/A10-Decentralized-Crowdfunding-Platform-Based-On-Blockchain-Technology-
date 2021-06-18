57a2a300a5eca80d5a74fce05ba0ce05c063c3de
benetagetzie <52785734+benetagetzie@users.noreply.github.com>
2021-06-18 18:11:49 +0530

    Source Code

---
+++ Decentralized Funding Platform Based on Blockchain Technology/build/web/donorhomenew.jsp
@@ -0,0 +1,34 @@
+<%-- 
+    Document   : adminhome
+    Created on : 4 Nov, 2020, 3:47:54 PM
+    Author     : h
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <title>Donor Home_page</title>
+        <style>
+body {
+ background-image: url("images/cover_bg_2.jpg");
+ background-color: #cccccc;
+}
+</style>
+    </head>
+    <body align ="center">
+       <div class="w3-container">
+           
+           <hr/>
+           <br/><br/><br/>
+           <a href="donorhome.jsp" class="w3-btn w3-black">Fund Requests</a>
+  <a href="fundhistn.jsp" class="w3-btn w3-black">Fund Transactions</a>
+  <br/><br/><br/>
+  <hr/>
+  
+ 
+</div>
+    </body>
+</html>
---
+++ Decentralized Funding Platform Based on Blockchain Technology/src/java/amount.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.commondb.Common_DB;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author h
+ */
+public class amount extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet amount</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet amount at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        
+       
+        
+        
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        
+         
+         String accno=request.getParameter("accno");    
+         String ccv=request.getParameter("ccv"); 
+         System.out.println(accno);
+         System.out.println(ccv);
+         
+         try
+         {
+             ResultSet rs=Common_DB.LoginCheck("crowdfund", "donor", "accno", "ccv", accno, ccv);
+             if(rs.next())
+             {
+                 response.sendRedirect("ft.jsp");
+             }
+             else
+             {
+                 response.sendRedirect("Error.jsp");
+             }
+         }
+         catch(SQLException | IOException e)
+         {
+             System.out.println(e);
+         } catch (Exception ex) {
+            Logger.getLogger(clog.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
